f1640280beec784ec9e7f6c871a84dea8d18b9db
TanayJyot <tjsinghchawla@gmail.com>
2024-06-29 19:42:15 +0530

    Pre commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,56 @@
+import 'package:buildspace_s5/order_tracking.dart';
 import 'package:buildspace_s5/models/user.dart';
 import 'package:buildspace_s5/screens/wrapper.dart';
 import 'package:buildspace_s5/services.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(const MyApp());
+  }
 
-  runApp(const MyApp());
-}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
+  static const LatLng destination = LatLng(37.33429383, -122.06600055);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // return MaterialApp(
+        // title: 'Flutter Demo',
+        // theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a purple toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+        //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        //   useMaterial3: true,
+        // ),
+    //     // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    //
     return StreamProvider<MyUser?>.value(
       value: AuthService().user,
       initialData: null,
@@ -32,4 +61,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
---
+++ lib/order_tracking.dart
@@ -0,0 +1,243 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+class OrderTrackingPage extends StatefulWidget {
+  final LatLng sourceLocation, destination;
+  const OrderTrackingPage(
+      {super.key, required this.sourceLocation, required this.destination});
+  @override
+  State<OrderTrackingPage> createState() => OrderTrackingPageState();
+}
+
+class OrderTrackingPageState extends State<OrderTrackingPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+  bool follow = true;
+
+  BitmapDescriptor sourceIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor destinationIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor currentLocationIcon = BitmapDescriptor.defaultMarker;
+  void setCustomMarkerIcon() {
+    BitmapDescriptor.asset(ImageConfiguration.empty, "assets/location pin.png")
+        .then(
+      (icon) {
+        setState(() {
+          sourceIcon = icon;
+        });
+      },
+    );
+    BitmapDescriptor.asset(ImageConfiguration.empty, "assets/location pin.png")
+        .then(
+      (icon) {
+        destinationIcon = icon;
+      },
+    );
+    BitmapDescriptor.asset(ImageConfiguration.empty, "assets/location pin.png")
+        .then(
+      (icon) {
+        currentLocationIcon = icon;
+      },
+    );
+  }
+
+  LocationData? currentLocation;
+  void getCurrentLocation() async {
+    Location location = Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        print("Location service not enabled");
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        print("location permission not granted");
+        return;
+      }
+    }
+
+    location.getLocation().then(
+      (location) {
+        setState(() {
+          currentLocation = location;
+        });
+      },
+    );
+
+    GoogleMapController googleMapController = await _controller.future;
+
+    location.onLocationChanged.listen((LocationData newLoc) {
+      currentLocation = newLoc;
+      if (follow) {
+        googleMapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              zoom: 13.5,
+              target: LatLng(
+                newLoc.latitude!,
+                newLoc.longitude!,
+              ),
+            ),
+          ),
+        );
+      }
+      location.enableBackgroundMode(enable: true);
+      setState(() {});
+    });
+  }
+
+  List<LatLng> polylineCoordinates = [];
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineRequest polylineRequest = PolylineRequest(
+        origin: PointLatLng(
+            widget.sourceLocation.latitude, widget.sourceLocation.longitude),
+        destination: PointLatLng(
+            widget.destination.latitude, widget.destination.longitude),
+        mode: TravelMode.driving);
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        request: polylineRequest,
+        googleApiKey: "AIzaSyDPU0Taag1cKPTbtV0awCqPjIYcwsOPx7Y");
+    if (result.points.isNotEmpty) {
+      result.points.forEach(
+        (PointLatLng point) => polylineCoordinates.add(
+          LatLng(point.latitude, point.longitude),
+        ),
+      );
+      setState(() {});
+    }
+  }
+
+  void _goToDelivery() async {
+    GoogleMapController googleMapController = await _controller.future;
+    if (currentLocation != null) {
+      googleMapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            zoom: 13.5,
+            target: LatLng(
+              currentLocation!.latitude!,
+              currentLocation!.longitude!,
+            ),
+          ),
+        ),
+      );
+    }
+    setState(() {
+      follow = true;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getPolyPoints();
+    setCustomMarkerIcon();
+    // getCurrentLocation();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height / 2,
+            width: double.maxFinite,
+            child: currentLocation == null
+                ? const Center(child: Text("Loading"))
+                : Listener(
+                    onPointerDown: (event) {
+                      if (follow) {
+                        setState(() {
+                          follow = false;
+                        });
+                      }
+                    },
+                    child: GoogleMap(
+                      // mapType: MapType.hybrid,
+                      myLocationButtonEnabled: false,
+                      initialCameraPosition: CameraPosition(
+                        target: LatLng(currentLocation!.latitude!,
+                            currentLocation!.longitude!),
+                        zoom: 13.5,
+                      ),
+                      markers: {
+                        Marker(
+                          markerId: const MarkerId("currentLocation"),
+                          position: LatLng(currentLocation!.latitude!,
+                              currentLocation!.longitude!),
+                          onTap: () => setState(() {
+                            follow = true;
+                          }),
+                          icon: currentLocationIcon,
+                        ),
+                        Marker(
+                          markerId: MarkerId("source"),
+                          position: widget.sourceLocation,
+                          infoWindow: const InfoWindow(
+                            title: "Restaurant",
+                            snippet: "Your food is picked up from here",
+                          ),
+                          icon: sourceIcon,
+                        ),
+                        Marker(
+                          markerId: MarkerId("destination"),
+                          position: widget.destination,
+                          infoWindow: const InfoWindow(
+                            title: "Destination",
+                            snippet: "Your food will be delivered here",
+                          ),
+                          icon: destinationIcon,
+                        ),
+                      },
+                      onMapCreated: (mapController) {
+                        _controller.complete(mapController);
+                      },
+                      polylines: {
+                        Polyline(
+                          polylineId: const PolylineId("route"),
+                          points: polylineCoordinates,
+                          color: const Color(0xFF7B61FF),
+                          width: 6,
+                        ),
+                      },
+                    ),
+                  ),
+          ),
+          Container(
+            width: double.maxFinite,
+            height: MediaQuery.sizeOf(context).height / 2,
+            color: Colors.green.shade500,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              "Your friend (and food) is on the way",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToDelivery,
+        label: const Text('Recenter'),
+        icon: const Icon(Icons.fastfood_rounded),
+      ),
+    );
+  }
+}
